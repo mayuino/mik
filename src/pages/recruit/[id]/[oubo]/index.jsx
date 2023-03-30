e12d6931eb40f8a9868d5fa56cc277e9bbb23458
mayuino <mendo.woker@gmail.com>
2023-03-30 16:27:50 +0900

    🔥 いらないコードを削除

--- src/pages/recruit/[id]/[oubo]/index.jsx
+++ src/pages/recruit/[id]/[oubo]/index.jsx
@@ -1,12 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
 import { Footer } from "@/components/Footer";
 import { Content_Oubo } from "@/components/Content/oubo";
 import HeadElement from "@/components/Head";
 import { useRouter } from "next/router";
-import { RecruitJobCard } from "@/components/Content/recruit_jobcard";
 import { SecondHeader } from "@/components/SecondHeader";
 
 const inter = Inter({ subsets: ["latin"] });
--- src/pages/recruit/[id]/index.jsx
+++ src/pages/recruit/[id]/index.jsx
@@ -1,4 +1,3 @@
-import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { Content_Recruit_Detail } from "@/components/Content/recruit_detail";
 import HeadElement from "@/components/Head";
