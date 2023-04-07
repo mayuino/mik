80f658ef485a4d434e8ab76ab1301f072b84ebae
mayuino <mendo.woker@gmail.com>
2023-04-07 17:41:12 +0900

    🎨 top画面修正

--- src/components/Top/index.jsx
+++ src/components/Top/index.jsx
@@ -3,14 +3,27 @@ import Image from 'next/image'
 import { Header } from '../Header'
 import { MenuBar } from '../MenuBar'
 import mobileStyles from '@/styles/mobile.module.css'
+import { useEffect, useState } from 'react'
 
 
 
 export const Top = () => {
+  const [isNotPhone, setIsNotPhone] = useState(false);
+
+  useEffect(() => {
+    const handleResize = ( ) => {
+      setIsNotPhone(window.innerWidth <= 900);
+    };
+    handleResize();
+    window.addEventListener('resize',handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  },[])
   return(
    
     <div className={`${styles.top_image_frame} ${mobileStyles.top_image_frame}`}>
       <Header/>
+      {isNotPhone ? null:(
+      <Image src="/sitting_man.png" alt="eigyou"  width={120} height={250} className={`${styles.sitting_image} ${mobileStyles.sitting_image}`}/>)}
       <MenuBar />
 
       </div>
